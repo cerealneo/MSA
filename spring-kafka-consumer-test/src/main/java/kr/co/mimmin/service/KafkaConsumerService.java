d4551f0a36e36271dbf01937b35b922f379a2d13
minsung <dpsdy1@gmail.com>
2025-04-09 11:20:14 +0900

    add

--- spring-kafka-consumer-test/src/main/java/kr/co/mimmin/service/KafkaConsumerService.java
+++ spring-kafka-consumer-test/src/main/java/kr/co/mimmin/service/KafkaConsumerService.java
@@ -13,17 +13,17 @@ public class KafkaConsumerService {
 
     @KafkaListener(topics = "my-topic-01",groupId = "group-my-topic-01")
     public void topic01Consumer(ConsumerRecord<String, String> record) {
-        log.info("topic01Consumer...");
+        log.info("topic01Consumer....");
         log.info(record.key()+ "-"+record.value());
     }
     @KafkaListener(topics = "my-topic-02",groupId = "group-my-topic-02")
     public void topic02Consumer(ConsumerRecord<String, String> record) {
-        log.info("topic02Consumer...");
+        log.info("topic02Consumer....");
         log.info(record.key()+ "-"+record.value());
     }
     @KafkaListener(topics = "my-topic-03",groupId = "group-my-topic-03")
     public void topic03Consumer(ConsumerRecord<String, String> record) {
-        log.info("topic03Consumer...");
+        log.info("topic03Consumer....");
         log.info(record.key()+ "-"+record.value());
     }
 }
